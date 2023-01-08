e4f09da6ba8995b7de1573aabedda38e18c749e1
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-08 13:50:45 +0100

    GT-12 ajouter le mot de passe pour configurer nodemailer

--- server/middleware/mailer.js
+++ server/middleware/mailer.js
@@ -7,13 +7,13 @@ const main = (method,user)=> {
     const token = jwt.sign({email:user.email},process.env.SECRET_TOCKEN)
     let subject = ''
     let html = ''
-           
+    
     if(method=='AddEmployé'){
-        subject = ' Tack your email and password '
+        subject = 'recevoire votre email et mot de passe'
         html = `<div style='height: 150px; width: 100%;'>
         <h4> 👋 Bonjour Voila votre données Bienvenu ! </h4>
-        <h5>Your email :${user.email}</h5>
-        <h5>Your password :${Storage('stockPassword')}</h5>
+        <h5>votre email :${user.email}</h5>
+        <h5>votre mot de passe :${Storage('stockPassword')}</h5>
         <p>You are receiving this because you (or someone else) have requested the reset of the password for your account. <span style='font-weight: bold;'>MARHABA</span>,Please click on the following link, or paste this into your browser to complete the process:</p>
         <a href="http://localhost:${3000}/path_login_frontend">Login</a> 
         ${Storage.clear()}
