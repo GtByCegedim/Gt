91890127fd531e02056e3d72738fe4beb232f0cf
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-09 15:11:44 +0100

    GT-12 instalation de packages

--- server/controllers/employéesController.js
+++ server/controllers/employéesController.js
@@ -5,13 +5,19 @@ const bcrypt = require("bcryptjs");
 const Generate_password_secure = require("secure-random-password");
 const Storage = require("local-storage");
 const ErrorResponse = require('../utils/error');
+// const user_roles = require("../models/index")
 
 const AddEmployé = async (req, res,next) => {
   const { body } = req;
-  if (!body.name || !body.email || !body.phone) return next(new ErrorResponse('Fill all filled',401));
+  if (!body.lastName || !body.email || !body.firstName) return next(new ErrorResponse('Fill all filled',401));
   else {
-    const findUser = await User.findOne({ email: body.email });
-    if (findUser) return next(new ErrorResponse('email a already existed',401));
+    const findUser = await User.findOne({
+      where: { 
+          email: body.email
+      }
+  })
+    // console.log(findUser)
+    if (findUser) return next(new ErrorResponse('il ya déja un Employé avec cet email',401));
     else {
       const stockPassword = Generate_password_secure.randomPassword({
         characters: [
@@ -20,15 +26,13 @@ const AddEmployé = async (req, res,next) => {
           Generate_password_secure.digits,
         ],
       });
-
       const generatePassword = await bcrypt.hash(stockPassword, 10);
-      const role_User = await Role.findOne({ name: "User" });
+      // const role_User = await Role.findOne({ name: "Employe" });
       const creatUser = await User.create({
         ...body,
         password: generatePassword,
-        role_id: role_User._id,
+        // role_id: role_User._id,
       });
-      
       if (!creatUser) return next(new ErrorResponse('error',401));
       Storage("stockPassword", stockPassword);
       mailer.main("AddEmployé", creatUser);
