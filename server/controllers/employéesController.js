f48e6210a25e35aca484262dd7695ca355b90f9c
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-09 17:18:46 +0100

    GT-12 ajouter le role lors de l acréation d'un employé

--- server/controllers/employéesController.js
+++ server/controllers/employéesController.js
@@ -5,19 +5,27 @@ const bcrypt = require("bcryptjs");
 const Generate_password_secure = require("secure-random-password");
 const Storage = require("local-storage");
 const ErrorResponse = require('../utils/error');
-// const user_roles = require("../models/index")
+const User_role = require('../models/user-role')
 
-const AddEmployé = async (req, res,next) => {
-  const { body } = req;
-  if (!body.lastName || !body.email || !body.firstName) return next(new ErrorResponse('Fill all filled',401));
+/**                 AJOUTER UN EMPLOYE
+ * It creates a user, then creates a role_user, then sends an email.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next function is used to pass control to the next matching route.
+ * @returns The user object.
+ */
+const AddEmployé = async (req, res, next) => {
+  const {
+    body
+  } = req;
+  if (!body.lastName || !body.email || !body.firstName) return next(new ErrorResponse('Fill all filled', 401));
   else {
     const findUser = await User.findOne({
-      where: { 
-          email: body.email
+      where: {
+        email: body.email
       }
-  })
-    // console.log(findUser)
-    if (findUser) return next(new ErrorResponse('il ya déja un Employé avec cet email',401));
+    })
+    if (findUser) return next(new ErrorResponse('il ya déja un Employé avec cet email', 401));
     else {
       const stockPassword = Generate_password_secure.randomPassword({
         characters: [
@@ -27,21 +35,30 @@ const AddEmployé = async (req, res,next) => {
         ],
       });
       const generatePassword = await bcrypt.hash(stockPassword, 10);
-      // const role_User = await Role.findOne({ name: "Employe" });
+      const role = await Role.findOne({
+        where: {
+          name: "employe"
+        }
+      });
       const creatUser = await User.create({
         ...body,
         password: generatePassword,
-        // role_id: role_User._id,
       });
-      if (!creatUser) return next(new ErrorResponse('error',401));
-      Storage("stockPassword", stockPassword);
-      mailer.main("AddEmployé", creatUser);
-      await res.send(creatUser);
+      if (!creatUser) return next(new ErrorResponse('user non creer ', 401));
+      else {
+        const role_id = role.id;
+        const user_id = creatUser.id
+        const role_user = await User_role.create({
+          userId: user_id,
+          roleId: role_id
+        })
+        if (!role_user) return next(new ErrorResponse('role_user non creer', 401));
+        Storage("stockPassword", stockPassword);
+        mailer.main("AddEmployé", creatUser);
+        await res.send(creatUser);
+      }
     }
   }
 };
 
-
-
 module.exports = AddEmployé
-
