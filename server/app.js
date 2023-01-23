4cc63ecc52da96fe1ed749307fedff426f56ec40
Badreddine-Ab <badreddine@gmail.com>
2023-01-23 16:47:55 +0100

    working on creating a team but having an error of router.use

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const globalError = require('./middleware/errorMiddleware')
 const employeRouter = require('./routes/employeRoute')
+const teamRoutes = require('./controllers/team');
 // Import database connection
 const {  DateType, Notification,Task,User, Role, Policy,  Subtask, TaskStatus } = require('./models');
 const sequelize = require('./config/database');
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 /* A middleware that is used to route the request to the employeRouter. */
 app.use('/api/employe',employeRouter)
+app.use('/api/teams', teamRoutes);
+
 // Set up routes, middleware, etc.
 app.use(globalError);
 
