cb0cf7615cc09047e73b191763b6c2b22938aae2
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-24 23:10:33 +0100

    GT-21 Completer le controlleur qui contient les fonctions pour l'ajout d'une task à un utulisateur on ajoutant les relations entre le projet et le type d'estimation

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -2,7 +2,6 @@ const Task = require('../models/task')
 const User = require('../models/user')
 const Project = require('../models/project')
 const status = require('../models/taskStatus')
-const Team = require('../models/team')
 const Subtask = require('../models/subtask')
 const DateType = require('../models/dateType')
 const TaskUser = require('../models/task_user')
@@ -10,7 +9,6 @@ const ErrorResponse = require('../utils/error')
 
 const addTaskToUser = async(req,res,next) =>{
   const {body} = req
-  const project_id = req.params.id
   try {
     if (!body.title || !body.description || !body.deadline || !body.duration || !body.unit) {
       return next(new ErrorResponse('Fill all filled', 401));
@@ -22,12 +20,14 @@ const addTaskToUser = async(req,res,next) =>{
       if(!addDateType) {
         return next(new ErrorResponse('Date Type Not created ', 401));
       }
+      const project_id = req.params.id
       const id_dateType =  addDateType.id
       const creatTask = await Task.create({
         title: body.title,
         description: body.description,
         deadline: body.deadline,
-        dateTypeId:  id_dateType
+        dateTypeId:  id_dateType,
+        projectId: project_id
       })
       if(!creatTask) {
         return next(new ErrorResponse('Task not created', 401));
