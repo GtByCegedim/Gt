39a1b20209db6748c80945082300bfd0c5573663
SafiaMaani <saf.maani@gmail.com>
2023-01-24 20:49:26 +0100

    Creation de la fonction forgetPassword

--- server/controllers/userAuthController.js
+++ server/controllers/userAuthController.js
@@ -4,6 +4,7 @@ const User_role = require('../models/user-role')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const apiError = require('../utils/error');
+const mailer = require('../middleware/mailer');
 
 
 const login = async (req, res, next) => {
@@ -24,7 +25,7 @@ const login = async (req, res, next) => {
 
   // Compare the provided password with the hashed password in the database
   const isMatch = await bcrypt.compare(password, user.password);
-  console.log(password, user.password);
+
   if (!isMatch) {
     return next(new apiError('Invalid credentials', 401));
   }
@@ -42,7 +43,38 @@ const login = async (req, res, next) => {
   });
 };
 
+const forgetPassword = async (req, res, next) => {
+  const {
+    email
+  } = req.body;
+  if (!email) {
+    return next(new apiError('Veuillez entrer votre email', 400))
+  }
+  // Find the user by email
+  const user = await User.findOne({
+    where: {
+      email
+    }
+  });
+  if (!user) {
+    return next(new apiError('User not found', 404));
+  }
+
+  // Send the reset password link to the user via email
+  mailer.main("forgetPassword",  user);
+  res.status(200).json({
+    success: true,
+    message: 'Reset password link sent'
+  });
+};
+
+const resetPassword = async (req, res, next) => {
+
+};
+
 
 module.exports = {
-  login
+  login,
+  forgetPassword,
+  resetPassword
 }
