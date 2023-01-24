1a7e042b7a304c5767fa67987bb22bc7f1f2fe86
SafiaMaani <saf.maani@gmail.com>
2023-01-24 20:48:01 +0100

    Ajout des routes forget et reset password

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,9 +2,12 @@ const express = require('express')
 const authRouter = express.Router()
 
 const {
-  login
+  login,
+  forgetPassword,
+  resetPassword
 } = require('../controllers/userAuthController')
 
 authRouter.post('/login', login);
-
+authRouter.post('/forgetpassword', forgetPassword)
+authRouter.post('/resetpassword', resetPassword)
 module.exports = authRouter
