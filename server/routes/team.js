5ec2b3202ccdb7817475b15a6781cc89d6ce45d4
Badreddine-Ab <badreddine@gmail.com>
2023-01-25 10:26:25 +0100

    deleting duplicated team file

--- server/routes/team.js
+++ server/routes/team.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const ErrorHandler = require('../middleware/errorMiddleware')
 
-const { createTeam, sendInvitations } = require("../controllers/team");
+const { createTeam, sendInvitations, findAllTeams } = require("../controllers/team");
 
 router.post('/create', createTeam);
 router.post('/invitations', sendInvitations);
+router.get('/all',findAllTeams)
 router.use(ErrorHandler)
 
 module.exports = router;
--- server/routes/teamRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { findAllTeams } = require('../controllers/teamController')
-const ErrorHandler = require('../middleware/errorMiddleware')
-const router = express.Router()
-
-router.get('/all',findAllTeams)
-router.use(ErrorHandler)
-module.exports = router
