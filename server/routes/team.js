7114c6b983629c56a02d6b296da941b35ce3759c
Badreddine-Ab <badreddine@gmail.com>
2023-01-27 16:58:18 +0100

    fixing the prod and dev modes, and deleting repeated variables in the exemple env, and fixing repeated require in team router

--- server/routes/team.js
+++ server/routes/team.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const ErrorHandler = require('../middleware/errorMiddleware')
 
-const { createTeam, findAllTeams, acceptInvitation, findAllTeams } = require("../controllers/team");
+const { createTeam,  acceptInvitation, findAllTeams } = require("../controllers/team");
 const { sendTeamInvitation } = require('../middleware/mailer');
 
 
