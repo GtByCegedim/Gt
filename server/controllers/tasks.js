583605b0b05c2e14e7fec0242bb5be054c7f457c
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-28 16:10:27 +0100

    GT-21 modification sur l afonction addTask pour que la tache puisse assignée a un ou plusieur employés

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -10,58 +10,90 @@ const Storage = require("local-storage");
 
 const ErrorResponse = require('../utils/error')
 
-const addTaskToUser = async(req,res,next) =>{
-  const {body} = req
+
+const addTaskToUser = async (req, res, next) => {
+  const manager = req.user
+  const {
+    body
+  } = req
   try {
-    if (!body.title || !body.description || !body.deadline || !body.duration || !body.unit) {
-      return next(new ErrorResponse('Fill all filled', 401));
-    }else {
+    if (!body.title || !body.description || !body.deadline || !body.duration || !body.unit || !body.users) {
+      console.log(body.users);
+      return next(new ErrorResponse('Fill all filled and users', 401));
+    } else {
+      // for (let i = 0; i < body.users.length; i++) {
+      //   const findUserByName = await User.findOne({
+      //     where: {
+      //       lastName: body.users[i].lastname,
+
+      //     }
+      //   });
+      //   if (!findUserByName) {
+      //     return next(new ErrorResponse(`User  ${body.users[i].lastname} not found`, 401));
+      //   }
+      // }
+      body.users.map(async (user) => {
+        const findUserByName = await User.findOne({
+          where: {
+            lastName: user.lastname
+          }
+        });
+        if (!findUserByName) {
+          return next(new ErrorResponse(`User  ${user.lastname} not found`, 401));
+        }
+      });
       const addDateType = await DateType.create({
-        duration : body.duration,
-        unit : body.unit
+        duration: body.duration,
+        unit: body.unit
       })
-      if(!addDateType) {
+      if (!addDateType) {
         return next(new ErrorResponse('Date Type Not created ', 401));
       }
+      const manager_id = manager.id
       const project_id = req.params.id
-      const id_dateType =  addDateType.id
+      const id_dateType = addDateType.id
       const creatTask = await Task.create({
         title: body.title,
         description: body.description,
         deadline: body.deadline,
-        dateTypeId:  id_dateType,
-        projectId: project_id
+        dateTypeId: id_dateType,
+        projectId: project_id,
+        manager: manager_id
       })
-      if(!creatTask) {
+      if (!creatTask) {
         return next(new ErrorResponse('Task not created', 401));
       }
-      const name_user = body.user_name
-      const findUserByName = await User.findOne({
-        name : name_user
-      })
-      if(!findUserByName) {
-        return next(new ErrorResponse('no user with this name', 401));
-      }
-      const UserId = findUserByName.id
-      const taskId = creatTask.id
-      const creatUserTask = await TaskUser.create({
-          userId : UserId,
+      body.users.map(async (user) => {
+        const name_luser = user.lastname
+        const findUserByName = await User.findOne({
+          where: {
+            lastName: name_luser,
+          }
+        })
+        if (!findUserByName) {
+          return next(new ErrorResponse(`no user with this name  ${name_luser}`, 401));
+        }
+        const UserId = findUserByName.id
+        const taskId = creatTask.id
+        const creatUserTask = await TaskUser.create({
+          userId: UserId,
           taskId: taskId
-      })
-      if(!creatUserTask) {
-        return next(new ErrorResponse('no relation user with task', 401));
-      }
-      console.log(creatTask.title)
-      Storage("creatTask", creatTask.title);
-      Storage("createdAt", creatTask.createdAt);
-      mailer.main("addTask", findUserByName);
-      await res.json(findUserByName);   
-     } 
+        })
+        if (!creatUserTask) {
+          return next(new ErrorResponse('no relation user with task', 401));
+        }
+        Storage("creatTask", creatTask.title);
+        Storage("createdAt", creatTask.createdAt);
+        mailer.main("addTask", findUserByName);
+      });
+      await res.json({
+        msg: "task added to users"
+      });
+    }
   } catch (error) {
-      return next(new ErrorResponse(error, 401));
+    return next(new ErrorResponse(error, 401));
   }
 }
-
 
 
 
