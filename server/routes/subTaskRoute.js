2e6b6dee0f1937c06efe5d7ad9d400f6db960559
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-31 11:48:27 +0100

    GT-40 Configuration des Routes pour  les controleurs qui meremttent la recuperation des utilisateurs connecter

--- server/routes/subTaskRoute.js
+++ server/routes/subTaskRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const  {addSubTask, getSubTaskOfTask} = require('../controllers/subTaskController');
+const  {addSubTask, getSubTaskOfTask, getMySubTasks} = require('../controllers/subTaskController');
+const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/:id', addSubTask)
 router.get('/allsubtasks/:id', getSubTaskOfTask)
+router.get('/mysubtasks', authMiddleware,getMySubTasks)
 
 module.exports = router;
