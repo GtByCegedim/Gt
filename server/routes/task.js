b274ad75536e501d200a50099e75208143d337d3
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-31 10:33:09 +0100

    GT-39 Configuration pour les Routes de controleur AllTaskOfMy

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { addTaskToUser, AllTaskOfProject,  AllTaskOfUser } = require('../controllers/tasks');
+const { addTaskToUser, AllTaskOfProject,  AllTaskOfUser, AllMyTask } = require('../controllers/tasks');
 const router = express.Router();
 const {authMiddleware} = require('../middleware/authMiddleware')
 /* This is a route that is being created. The first parameter is the path, the second is the function
 that will be called when the route is hit. */
 router.post('/add/:id',authMiddleware,addTaskToUser);
+router.get('/my',authMiddleware,AllMyTask);
 router.get('/:id',AllTaskOfProject);
-router.get('/',AllTaskOfUser);
+// router.get('/',AllTaskOfUser);
+
 module.exports = router;
