6c6dd33768fe84f9c8bf0a3883e8e674fb667a26
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-01 10:50:12 +0100

    GT-43 ajouter les controleur de modification de statut

--- server/controllers/statusController.js
+++ server/controllers/statusController.js
@@ -1,20 +1,23 @@
 
 const Statut = require('../models/status')
+const Task = require('../models/task')
+const Task_statut = require('../models/task_statut')
+const Project = require('../models/project')
+const ErrorResponse = require('../utils/error');
+
 
 
 // This function adds a new status to a project
-const addNewStatut = async(res,req,next) => {
+const addNewStatut = async(req,res,next) => {
     // Retrieve the request body
-    const req = {
-        body
-    }
+   const name = req.body.name
     // Get the manager from the request user object
     const manager = req.user
     // Get the project ID from the request params
     const project_id = req.params.id
     try {
         // Check if the status field is present in the request body
-        if(!body.status){
+        if(!name){
             // If not, throw an error
             return next(new ErrorResponse('fialled ad filled', 401));
         }
@@ -38,10 +41,10 @@ const addNewStatut = async(res,req,next) => {
           }
           // Create the new status
           const NewStatut = await Statut.create({
-            status : body.status
+            status : name
           })
           // Check if the status was created successfully
-          if(NewStatut) {
+          if(!NewStatut) {
             // If not, throw an error
             return next(new ErrorResponse('Statut not created', 401));
           }
@@ -51,14 +54,93 @@ const addNewStatut = async(res,req,next) => {
           })
     } catch (error) {
         // Handle any errors that occur during the process
-        return next(new ErrorResponse("No tasks found for project", 404));
+        return next(new ErrorResponse(error, 404));
 
     }
 }
 
+const deletStatus = async(req,res,next)=>{
+    const manager = req.user
+    // Get the project ID from the request params
+    const project_id = req.params.idProject
+    try {
+        // Check if the status field is present in the request body
+        if(!body.status){
+            // If not, throw an error
+            return next(new ErrorResponse('fialled ad filled', 401));
+        }
+        // Get the manager ID
+        const manager_id = manager.id
+        // Get the project with the specified ID
+        const getProject = await Project.findOne({
+            where : {
+              id: project_id
+            }
+          })
+          // Check if the project exists
+          if (!getProject) {
+            // If not, throw an error
+            return next(new ErrorResponse('Sory No Project Found', 401));
+        }
+          if(getProject.manager !== manager_id){
+                // If not, throw an error
+                return next(new ErrorResponse('Sory You Are Not Manager Of this Project', 401));
+        }
+        const idStatut = req.params.idStatut
+        const deletStatut = await Statut.delete({
+            id : idStatut
+        })
+        if(!deletStatut){
+            return next(new ErrorResponse('Sory ', 401));
+        }
+        res.json({
+            messagme : "statut deleted"
+        })
+    } catch{
+        return next(new ErrorResponse(error, 401));
+    }
+
+}
+
+const updateStatusOfTask = async(req,res,next) =>{
+    const task_id = req.params.id
+    const statut = req.body.statut
+    try {
+        const findTask = await Task.findByPk(task_id)
+        if(!findTask){
+            return next(new ErrorResponse("No task Found", 401));
+        }
+        const findStatut = await Statut.findOrCreate({
+            where : {
+                status : statut
+            }
+        })
+        console.log(findStatut[0].id)
+        if(!findStatut){
+            return next(new ErrorResponse('No task found or create', 401));
+        }
+        const task_Status = await  Task_statut.update({
+            statusId : findStatut[0].id
+        },
+        {
+            where : {
+                taskId : task_id
+            }
+        })
+        if(!task_Status){
+            return next(new ErrorResponse('No relation', 401));
+        }
+        res.json({
+            message : `New Status of task ${findTask.title}  :  ${statut}`
+        })
+    } catch (error) {
+        return next(new ErrorResponse(error, 401));
+    }
+}
 
 
 module.exports = {
     addNewStatut,
-    deletStatus
+    deletStatus,
+    updateStatusOfTask
 }
