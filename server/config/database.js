efdd067cdcbce92bc374a2afbd439900dfa9567a
Badreddine-Ab <badrestronger@gmail.com>
2023-02-07 11:57:44 +0100

    fixing the docker compose

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.password, {
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize('postgres://bader:2001@postgres:5432/GT');
 
 sequelize
   .authenticate()
