754ae21139ef720a940cb4f87328b72499d840ef
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-12 19:52:49 +0100

    ajouter le route pour  le controlleur qui permet de recuperer tous les projets

--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createProject, UpdateProject } = require('../controllers/projectController');
+const { createProject, UpdateProject, findALLprojects } = require('../controllers/projectController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/add',authMiddleware, createProject)
 router.put('/update/:id',authMiddleware, UpdateProject)
-
+router.get('/all',findALLprojects)
 
 module.exports = router;
