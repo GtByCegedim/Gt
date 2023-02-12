bc58748a63d695a9467aba703bd12344ee3c9fcb
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-12 21:18:17 +0100

    ajouter le route pour le controlleur BaneProject

--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createProject, UpdateProject, findALLprojects } = require('../controllers/projectController');
+const { createProject, UpdateProject, findALLprojects, OnlyMyProjects, baneProject } = require('../controllers/projectController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/add',authMiddleware, createProject)
 router.put('/update/:id',authMiddleware, UpdateProject)
-router.get('/all',findALLprojects)
-
+router.get('/all',authMiddleware,findALLprojects)
+router.get('/all/my',authMiddleware,OnlyMyProjects)
+router.put('/bane/:id',authMiddleware,baneProject)
 module.exports = router;
