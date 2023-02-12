77c6e7cf09c2f7e31a7ad36b22fc59135532d663
Badreddine-Ab <badrestronger@gmail.com>
2023-02-12 16:43:53 +0100

    adding tailwind to the vue app

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-import './assets/main.css'
+import './assets/style.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
