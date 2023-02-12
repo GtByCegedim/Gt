e7068ac45e96aa2844f65edbc6a787ba8b067a7e
Badreddine-Ab <badrestronger@gmail.com>
2023-02-12 19:17:29 +0100

    adding the projects table

--- client/src/components/dashboard/Projects.vue
+++ client/src/components/dashboard/Projects.vue
@@ -1,3 +1,38 @@
+<script>
+import VTooltip from "v-tooltip";
+
+export default {
+  name: "ProjectInformation",
+  directives: {
+    tooltip: VTooltip.directive,
+  },
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          name: "Project 1",
+          creator: "John Doe",
+          members: "Jane Doe, Bob Smith",
+        },
+        {
+          id: 2,
+          name: "Project 2",
+          creator: "Jane Doe",
+          members: "John Doe, Bob Smith",
+        },
+        {
+          id: 3,
+          name: "Project 3",
+          creator: "Bob Smith",
+          members: "John Doe, Jane Doe",
+        },
+      ],
+    };
+  },
+};
+</script>
+
 <template>
   <div class="flex-1 p-6">
     <h2 class="text-2xl font-bold mb-4">List of Projects</h2>
@@ -5,7 +40,6 @@
       <table class="w-full text-left table-collapse">
         <thead>
           <tr>
-            <th class="text-sm font-medium p-2 bg-gray-100 text-gray-600">#</th>
             <th class="text-sm font-medium p-2 bg-gray-100 text-gray-600">
               Project Number
             </th>
@@ -36,15 +70,25 @@
                 {{ project.creator }}
               </div>
             </td>
-            <td class="p-2 border-t">{{ project.projectNumber }}</td>
             <td class="p-2 border-t">
-  <template v-for="member in project.members.split(', ')">
-    <div class="bg-gray-300 text-gray-700 rounded-full inline-block p-2 hover:bg-gray-400" v-tooltip="member">{{ member.charAt(0) + member.charAt(member.length - 1) }}</div>
-  </template>
-  <div class="bg-gray-300 text-gray-700 rounded-full inline-block p-2 hover:bg-gray-400" v-tooltip="'Add Member'">
-    <i class="fas fa-plus"></i>
-  </div>
-</td>
+              <template v-for="member in project.members.split(', ')">
+                <div
+                  class="bg-gray-300 text-gray-700 rounded-full inline-block p-2 hover:bg-gray-400"
+                  v-tooltip="member"
+                >
+                  {{
+                    member.split(" ")[0].charAt(0) +
+                    member.split(" ")[1].charAt(0)
+                  }}
+                </div>
+              </template>
+              <div
+                class="bg-gray-300 text-gray-700 rounded-full inline-block p-2 hover:bg-gray-400"
+                v-tooltip="'Add Member'"
+              >
+                <i class="fas fa-plus"></i>
+              </div>
+            </td>
             <td class="p-2 border-t">
               <button
                 class="bg-gray-300 text-gray-700 p-2 rounded hover:bg-gray-400"
@@ -72,33 +116,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "ProjectInformation",
-  data() {
-    return {
-      projects: [
-        {
-          id: 1,
-          name: "Project 1",
-          creator: "John Doe",
-          members: "Jane Doe, Bob Smith",
-        },
-        {
-          id: 2,
-          name: "Project 2",
-          creator: "Jane Doe",
-          members: "John Doe, Bob Smith",
-        },
-        {
-          id: 3,
-          name: "Project 3",
-          creator: "Bob Smith",
-          members: "John Doe, Jane Doe",
-        },
-      ],
-    };
-  },
-};
-</script>
