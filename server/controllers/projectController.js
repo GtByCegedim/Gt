e5ff5e1bc610c58a08963c52e9f0b08588144fc4
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-12 14:13:21 +0100

    modification

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,24 +1,25 @@
 const Project = require('../models/project.js');
-const Team = require('../models/team');
 const apiError = require('../utils/error.js');
 
 exports.createProject = async (req, res) => {
+    const manager_id = req.user.id
     try {
-        const { name, description, groupId } = req.body;
+        const { name, description ,deadline } = req.body;
         const project = await Project.create({
             name,
-            description
+            description,
+            deadline,
+            manager : manager_id
         });
-
-        const team = await Team.findByPk(teamId);
-        await project.setGroup(team);
-
+        if(!project){
+        return next(new apiError("Error creating project", 500))
+        }
         res.status(201).json({
             message: "Project created successfully",
             project
         });
     } catch (error) {
         console.log(error);
-        return next(new apiError("Error creating project", 500))
+        return next(new apiError(error, 500))
     }
 };
