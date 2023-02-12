a715e43f6586dfe00b30cc4e17ab03c938da5727
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-12 18:11:06 +0100

    ajouter le controlleur qui permet de modifier sur un projet via son id

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,18 +1,32 @@
 const Project = require('../models/project.js');
 const apiError = require('../utils/error.js');
+const projctUsers = require('../models/project_user');
+const User = require('../models/user.js');
 
-exports.createProject = async (req, res) => {
+exports.createProject = async (req, res, next) => {
     const manager_id = req.user.id
     try {
-        const { name, description ,deadline } = req.body;
+        const {
+            name,
+            description,
+            deadline
+        } = req.body;
+        const findProjectByName = await Project.findOne({
+            where: {
+                name: name
+            }
+        })
+        if (findProjectByName) {
+            return next(new apiError("plez enter Name project again", 500))
+        }
         const project = await Project.create({
             name,
             description,
             deadline,
-            manager : manager_id
+            manager: manager_id
         });
-        if(!project){
-        return next(new apiError("Error creating project", 500))
+        if (!project) {
+            return next(new apiError("Error creating project", 500))
         }
         res.status(201).json({
             message: "Project created successfully",
@@ -23,3 +37,63 @@ exports.createProject = async (req, res) => {
         return next(new apiError(error, 500))
     }
 };
+
+exports.UpdateProject = async (req, res, next) => {
+    const project_id = req.params.id
+    const manager_id = req.user.id
+    const ids = []
+    const {
+        name,
+        description,
+        deadline
+    } = req.body
+    try {
+        const findProjectByName = await Project.findOne({
+            where: {
+                name: name
+            }
+        })
+        if (findProjectByName) {
+            return next(new apiError("plez enter Name project again", 500))
+        }
+        const chackProject = await Project.findOne({
+            where: {
+                id: project_id
+            }
+        })
+        if (!chackProject) {
+            return next(new apiError("no project found", 400))
+
+        }
+        if (manager_id != chackProject.manager) {
+            return next(new apiError("Sorry , You are Not manager Of this Project", 400))
+        }
+        const UpdateProject = await Project.update({
+            name,
+            description,
+            deadline
+        }, {
+            where: {
+                id: project_id
+            }
+        })
+        if (!UpdateProject) {
+            return next(new apiError("Error Updating project", 500))
+        }
+        const findUser = await projctUsers.findAll({
+            where : {
+                projectId : project_id
+            }
+        })
+        if(findUser){
+           console.log(findUser);
+        }
+        res.json({
+            message : "Project Updating",
+            
+        })
+    } catch (error) {
+        return next(new apiError(error, 500))
+
+    }
+}
--- server/routes/projectRoute.js
+++ server/routes/projectRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createProject } = require('../controllers/projectController');
+const { createProject, UpdateProject } = require('../controllers/projectController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/add',authMiddleware, createProject)
+router.put('/update/:id',authMiddleware, UpdateProject)
 
 
 module.exports = router;
