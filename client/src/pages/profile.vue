0994374ec0d8b320a0af08966417c6d6aadf5a89
Badreddine-Ab <badrestronger@gmail.com>
2023-02-14 22:25:14 +0100

    adding more fake data

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -1,62 +1,72 @@
 <template>
   <div>
     <h3 class="text-2xl font-bold text-left py-2">Kanban Board</h3>
-  </div>
-  
-  <div class="flex flex-row space-x-4">
-    <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
-      <h3
-        class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300"
-      >
-        To Do
-      </h3>
-      <div class="space-y-2">
-        <div
-          v-for="task in toDo"
-          :key="task.id"
-          class="p-2 bg-white shadow-md rounded-lg cursor-move"
-        >
-          <h4 class="font-medium">{{ task.title }}</h4>
-          <p class="text-gray-500 text-sm">{{ task.description }}</p>
+
+    <div class="flex flex-row space-x-4">
+      <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
+        <h3 class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300">
+          To Do
+        </h3>
+        <div class="space-y-2">
+          <div
+            v-for="task in toDo"
+            :key="task.id"
+            class="p-2 bg-white shadow-md rounded-lg cursor-move"
+            draggable="true"
+          >
+            <h4 class="font-medium">{{ task.name }}</h4>
+            <p class="text-gray-500 text-sm">{{ task.description }}</p>
+            <div class="flex flex-row justify-between">
+              <span class="text-gray-500 text-xs">{{ task.assignedTo }}</span>
+              <span class="text-gray-500 text-xs">{{ task.timeEstimate }}</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
-      <h3
-        class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300"
-      >
-        Doing
-      </h3>
-      <div class="space-y-2">
-        <div
-          v-for="task in doing"
-          :key="task.id"
-          class="p-2 bg-white shadow-md rounded-lg cursor-move"
-        >
-          <h4 class="font-medium">{{ task.title }}</h4>
-          <p class="text-gray-500 text-sm">{{ task.description }}</p>
+      <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
+        <h3 class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300">
+          Doing
+        </h3>
+        <div class="space-y-2">
+          <div
+            v-for="task in doing"
+            :key="task.id"
+            class="p-2 bg-white shadow-md rounded-lg cursor-move"
+            draggable="true"
+          >
+            <h4 class="font-medium">{{ task.name }}</h4>
+            <p class="text-gray-500 text-sm">{{ task.description }}</p>
+            <div class="flex flex-row justify-between">
+              <span class="text-gray-500 text-xs">{{ task.assignedTo }}</span>
+              <span class="text-gray-500 text-xs">{{ task.timeEstimate }}</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
-      <h3
-        class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300"
-      >
-        Done
-      </h3>
-      <div class="space-y-2">
-        <div
-          v-for="task in done"
-          :key="task.id"
-          class="p-2 bg-white shadow-md rounded-lg cursor-move"
-        >
-          <h4 class="font-medium">{{ task.title }}</h4>
-          <p class="text-gray-500 text-sm">{{ task.description }}</p>
+      <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
+        <h3 class="text-2xl font-bold text-left py-2 mb-4 border-b-2 border-gray-300">
+          Done
+        </h3>
+        <div class="space-y-2">
+          <div
+            v-for="task in done"
+            :key="task.id"
+            class="p-2 bg-white shadow-md rounded-lg cursor-move"
+            draggable="true"
+          >
+            <h4 class="font-medium">{{ task.name }}</h4>
+            <p class="text-gray-500 text-sm">{{ task.description }}</p>
+            <div class="flex flex-row justify-between">
+              <span class="text-gray-500 text-xs">{{ task.assignedTo }}</span>
+              <span class="text-gray-500 text-xs">{{ task.timeEstimate }}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -65,27 +75,57 @@ export default {
       tasks: [
         {
           id: 1,
-          title: "Create wireframes",
-          description: "Lorem ipsum dolor sit amet",
-          status: "to-do",
+          name: 'Task 1',
+          description: 'This is task 1',
+          priority: 'high',
+          assignedTo: 'John Doe',
+          timeEstimate: '2 hours',
+          status: 'to-do',
         },
         {
           id: 2,
-          title: "Design homepage",
-          description: "Consectetur adipiscing elit",
-          status: "to-do",
+          name: 'Task 2',
+          description: 'This is task 2',
+          priority: 'medium',
+          assignedTo: 'Jane Smith',
+          timeEstimate: '1 hour',
+          status: 'to-do',
         },
         {
           id: 3,
-          title: "Develop backend",
-          description: "Sed do eiusmod tempor incididunt",
-          status: "doing",
+          name: 'Task 3',
+          description: 'This is task 3',
+          priority: 'low',
+          assignedTo: 'Bob Johnson',
+          timeEstimate: '30 minutes',
+          status: 'doing',
         },
         {
           id: 4,
-          title: "Write documentation",
-          description: "Ut enim ad minim veniam",
-          status: "done",
+          name: 'Task 4',
+          description: 'This is task 4',
+          priority: 'high',
+          assignedTo: 'Sara Lee',
+          timeEstimate: '3 hours',
+          status: 'doing',
+        },
+        {
+          id: 5,
+          name: 'Task 5',
+          description: 'This is task 5',
+          priority: 'medium',
+          assignedTo: 'David Kim',
+          timeEstimate: '2 hours',
+          status: 'done',
+        },
+        {
+          id: 6,
+          name: 'Task 6',
+          description: 'This is task 6',
+          priority: 'low',
+          assignedTo: 'Amy Chen',
+          timeEstimate: '1 hour',
+          status: 'done',
         },
       ],
     };
