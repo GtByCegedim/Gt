3f31af57d2dbc5d5f9225cbe97e50cd419e81ad5
SafiaMaani <saf.maani@gmail.com>
2023-02-21 21:51:32 +0100

    page about

---
+++ client/src/user/component/navbar.vue
@@ -0,0 +1,32 @@
+<template>
+      <nav
+      class="fixed z-20 w-full bg-white/80 dark:bg-gray-900/80 backdrop-blur navbar shadow-md shadow-gray-600/5 peer-checked:navbar-active md:relative md:bg-transparent dark:shadow-none"
+      style="background-color: #2c5681"
+    >
+      <div class="xl:container m-auto px-6 md:px-12">
+        <div
+          class="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0"
+        >
+          <div class="w-full flex justify-between lg:w-auto"></div>
+          <div
+            class="navmenu hidden w-full flex-wrap justify-end items-center mb-16 space-y-8 p-6 border border-gray-100 rounded-3xl shadow-2xl shadow-gray-300/20 bg-white dark:bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none dark:shadow-none dark:border-gray-700 lg:border-0"
+          >
+            <div
+              class="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l"
+            >
+              <a
+                href="login"
+                class="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full focus:before:bg-primary/10 dark:focus:before:bg-primaryLight/10 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
+                style="color: white"
+              >
+                <span
+                  class="relative text-sm font-semibold text-primary text-primaryLight"
+                  >Login</span
+                >
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+</template>
---
+++ client/src/user/master/about.vue
@@ -0,0 +1,24 @@
+<script>
+import navbar from "../component/navbar.vue";
+import firstComponent from "../component/firstComponent.vue";
+import secondComponent from "../component/secondComponent.vue";
+import thirdComponent from '../component/thirdComponent.vue'
+
+export default {
+  components: {
+    "v-navbar": navbar,
+    "v-component1": firstComponent,
+    "v-component2": secondComponent,
+    "v-component3": thirdComponent,
+  },
+};
+</script>
+<template>
+  <header></header>
+  <div>
+    <v-navbar></v-navbar>
+    <v-component1></v-component1>
+    <v-component2></v-component2>
+    <v-component3></v-component3>
+  </div>
+</template>
