747b1a50d6f8fbe09de889857f53318094fdaaaf
SafiaMaani <saf.maani@gmail.com>
2023-02-22 19:19:32 +0100

    modification du point d'entrée

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 // Load environment variables from .env file
 require("dotenv").config();
-
 // Import other dependencies
 const express = require("express");
 const globalError = require("./middleware/errorMiddleware");
@@ -11,12 +10,11 @@ const authRouter = require("./routes/authRouter");
 const subTaskRouter = require("./routes/subTaskRoute");
 const statutouter = require("./routes/statutRoute");
 const swaggerAutogen = require("swagger-autogen")();
-const swaggerDocument = require("./swagger.json");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const ProjectRouter = require("./routes/projectRoute");
-
 const outputFile = "./swagger.json";
-const endpointsFiles = ["./routes/*.js", "app.js"];
+const endpointsFiles = ["./route/*.js", "./app.js"];
 // Import database connection
 const {
   DateType,
@@ -36,10 +34,8 @@ createAdmin();
 //Import creation Roles
 const createRole = require("./config/role");
 createRole();
-
 // Set up Express app
 const app = express();
-
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
@@ -88,7 +84,6 @@ const options = {
 };
 swaggerAutogen(outputFile, endpointsFiles, options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 // Route d'authentification:
 app.use("/api/auth", authRouter);
 app.use("/api/employe", employeRouter);
@@ -97,15 +92,12 @@ app.use("/api/task", taskRouter);
 app.use("/api/subtask", subTaskRouter);
 app.use("/api/status", statutouter);
 app.use("/api/project", ProjectRouter);
-
 // Set up routes, middleware, etc.
 app.use(globalError);
-
 // Start the server
 const server = app.listen(process.env.PORT, () => {
   console.log("Server is listening on port" + process.env.PORT);
 });
-
 // Handle errors outside express
 process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors : ${err.name} | ${err.message}`);
