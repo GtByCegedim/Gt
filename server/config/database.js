6d147392d15c2a5e839dd7907e57f53bbbbf8d9b
SafiaMaani <saf.maani@gmail.com>
2023-02-24 09:52:43 +0100

    Modification cnx DB

--- server/config/database.js
+++ server/config/database.js
@@ -1,30 +1,22 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.password,
-  {
-    dialect: "postgres",
-    logging: false,
-  }
-);
+
+const sequelize = new Sequelize('postgres://bader:2001@postgres:5432/GT');
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log(
-      "Connection to the database has been established successfully."
-    );
+    console.log('Connection to the database has been established successfully.');
 
     // Create the tables for the models
     return sequelize.sync();
   })
   .then(async () => {
-    console.log("Tables created successfully.");
+    console.log('Tables created successfully.');
   })
   .catch((error) => {
-    console.error("Error creating tables:", error);
+    console.error('Error creating tables:', error);
   });
+
 
 module.exports = sequelize;
