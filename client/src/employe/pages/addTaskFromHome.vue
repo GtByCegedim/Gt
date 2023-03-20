dd951d2b7824665cb715e74bc159996bc5b44bc6
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-20 21:32:47 +0000

    ajouter le composant pour l ajout d une tache de n importe ou

---
+++ client/src/employe/pages/addTaskFromHome.vue
@@ -0,0 +1,40 @@
+<template>
+	<section class="p-6 dark:bg-gray-800 h-full dark:text-gray-50">
+		<form  class="container flex flex-col mx-auto space-y-12 ng-untouched max-h-full ng-pristine ng-valid">
+			<fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+				<div class="space-y-2 col-span-full lg:col-span-1">
+					<p class="font-medium">Choisissez le Projet </p>
+					<p class="text-xs">et Ajouter une nouvele tache</p>
+				</div>
+				<div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+					<div class="col-span-full sm:col-span-3">
+						<label for="firstname" class="text-sm">First name</label>
+						<input id="firstname" type="text" placeholder="First name" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+					
+					<div class="col-span-full sm:col-span-3">
+						<label for="email" class="text-sm">Email</label>
+						<input id="email" type="email" placeholder="Email" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+					<div class="col-span-full">
+						<label for="address" class="text-sm">Derscription</label>
+						<input id="address" type="text" placeholder="" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+					<div class="col-span-full sm:col-span-2">
+						<label for="city" class="text-sm">durée</label>
+						<input id="city" type="text" placeholder="" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+					<div class="col-span-full sm:col-span-2">
+						<label for="state" class="text-sm">Heures / Points</label>
+						<input id="state" type="text" placeholder="" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+					<div class="col-span-full sm:col-span-2">
+						<label for="zip" class="text-sm">Projet</label>
+						<input id="zip" type="text" placeholder="" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900">
+					</div>
+				</div>
+			</fieldset>
+		
+		</form>
+	</section>
+</template>
