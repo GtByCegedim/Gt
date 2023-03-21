13a87aaafe9a20e58c4f29e5da138b702121b711
Badreddine-AB <badreddine@gmail.com>
2023-03-21 15:41:36 +0000

    fixing the databse connexion

--- server/config/database.js
+++ server/config/database.js
@@ -1,24 +1,24 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.password}@postgres:5432/${process.env.DATABASE}`);
-
+const sequelize = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.password}@postgres:5432/${process.env.DATABASE}`
+);
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection to the database has been established successfully.');
+    console.log(
+      "Connection to the database has been established successfully."
+    );
 
     // Create the tables for the models
-    return sequelize.sync({alert:true});
+    return sequelize.sync({ alert: true });
   })
   .then(async () => {
-    console.log('Tables created successfully.');
+    console.log("Tables created successfully.");
   })
   .catch((error) => {
-    console.error('Error creating tables:', error);
-    
+    console.error("Error creating tables:", error);
   });
-
 
 module.exports = sequelize;
